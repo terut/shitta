4b58e5fa4cc89f2be16abb91a2e1a0034010ed0c
terut <terut.dev+github@gmail.com>
2012-12-11 01:43:00 +0900

    Change null false for name

--- db/migrate/20121114100035_create_users.rb
+++ db/migrate/20121114100035_create_users.rb
@@ -4,11 +4,10 @@ class CreateUsers < ActiveRecord::Migration
       t.string :username, null: false
       t.string :email, null: false
       t.string :password_digest, null: false
-      t.string :name
+      t.string :name, null: false
       t.text :bio
 
       t.timestamps
-
     end
 
     change_table :users do |t|
