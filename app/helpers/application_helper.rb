c17fa07f86812e68ae95868f815ad43b29e6726c
terut <terut.dev+github@gmail.com>
2012-12-23 18:18:17 +0900

    Add show note with syntax highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  class Pygmentize < Redcarpet::Render::HTML
+    include Redcarpet::Render::SmartyPants
+
+    def block_code(code, language)
+      <<-EOS
+        <div class="code-container">
+          <div class="code-title">hogehoge</div>
+          #{highlight(code, language)}
+        </div>
+      EOS
+    end
+
+    def highlight(code, language)
+      Pygments.highlight(code,
+                         lexer: language,
+                         options: { linenos: true, encoding: 'utf-8'})
+    end
+  end
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(
+      Pygmentize.new(filter_html: true, hard_wrap: true),
+      no_intra_emphasis: true,
+      tables: true,
+      fenced_code_blocks: true,
+      autolink: true,
+      strikethrough: true,
+      lax_spacing: true,
+      space_after_headers: true,
+      superscript: true
+    )
+    markdown.render(text).html_safe
+  end
 end
