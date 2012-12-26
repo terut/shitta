d7c6c584500ce1187fb0852c03411f7d36bd5bc2
terut <terut.dev+github@gmail.com>
2012-12-27 02:53:36 +0900

    Add SignedInConstraint for root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
+class SignedInConstraint
+  def self.matches?(request)
+    request.cookies.key?('user_token')
+  end
+end
+
 Shitta::Application.routes.draw do
   get 'signin' => "sessions#new"
   get 'signup' => "users#new"
 
-  root :to => 'notes#index'
+  root to: 'notes#index', constraints: SignedInConstraint
+  root to: 'sessions#welcome'
 
   resources :users
-  resources :sessions
   resources :notes
   resources :services
+  resources :sessions
 end
