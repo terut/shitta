0ae5f04b2841785d7072bb2e0847c85ab56d1fbe
terut <terut.dev+github@gmail.com>
2013-05-04 19:31:50 +0900

    Add comments create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @note = Note.find(params[:note_id])
+    @comment = current_user.comments.build(params[:comment])
+    @comment.note = @note
+    @comment.save
+    redirect_to note_path(@note)
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,5 @@
 class Note < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   attr_accessible :raw_body, :title
 end
