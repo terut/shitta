d4c5911cb9a9da1e61a6f8bedf2915d5ff87a32e
terut <terut.dev+github@gmail.com>
2013-05-21 01:00:30 +0900

    Change username and password validation options

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,8 @@ class User < ActiveRecord::Base
   has_many :comments
 
   # TODO review validates and spec
-  validates :username, presence: true, format: { with: /\A[a-z0-9_]+\z/ }, length: { maximum: 20 }, uniqueness: true, on: :create
-  validates :password, presence: true, length: { maximum: 20 }, on: :create
-  validates :password_confirmation, presence: true, length: { maximum: 20 }, on: :create
+  validates :username, format: { with: /\A[a-z0-9_]+\z/ }, length: { maximum: 20 }, uniqueness: true, on: :create
+  validates :password, confirmation: true, length: { minimum: 4, maximum: 20 }, on: :create
   validates :email, email: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 20 }
   validates :bio, presence: true, length: { maximum: 400 }, on: :update
