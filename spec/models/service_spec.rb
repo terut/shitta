9462c6d1d8fdc53791917a0b6bea9e6feaa875bd
terut <terut.dev+github@gmail.com>
2013-05-26 20:06:18 +0900

    Add qitta connect spec

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Service do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#save_with_api' do
+    let(:service) { create(:user).services.build }
+    subject { service.save_with_api('test', 'test') }
+
+    context 'qiita user is collect' do
+      before do
+        Qiita.stub(:new) { double('qiita', url_name: 'test', token: '098f6bcd4621d373cade4e832627b4f6') }
+      end
+
+      it { should be_true }
+    end
+
+    context 'qiita user is not collect' do
+      before do
+        Qiita.stub(:new) { raise Qiita::BadRequest }
+      end
+
+      it { should be_false }
+    end
+  end
 end
