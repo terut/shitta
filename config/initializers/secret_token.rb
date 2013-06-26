224a1481281e330113a784148895dbd45c556cc0
terut <terut.dev+github@gmail.com>
2013-06-26 13:20:28 +0900

    Add config.secret_key_base setting

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Shitta::Application.config.secret_token = '8b6b5f1fc35facb3ff81fe8f86803e3e38117063a343c467584d07ba01d07ea4163a02e61265c7a434b6ea6c33ced832ecc010b94bdb0d24dc971f753fd55124'
+Shitta::Application.config.secret_key_base = 'i8ACx2Vj'
