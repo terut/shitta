9e14f83a252616b4843c9c5361c213e7a123b7d7
terut <terut.dev+github@gmail.com>
2014-02-06 02:16:15 +0900

    Add unicorn config

---
+++ config/unicorn/production.rb
@@ -0,0 +1,26 @@
+worker_processes 2
+
+working_directory "/var/www/vhosts/shitta"
+
+listen '/tmp/unicorn_of_shitta.sock'
+
+stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  unless old_pid == server.pid
+    begin
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
