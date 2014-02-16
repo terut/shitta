81f550827b946f37bff9b24f87cb360b2e277282
terut <terut.dev+github@gmail.com>
2014-02-17 02:05:02 +0900

    Add syntax.css to precompile css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,7 @@ Shitta::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( syntax.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
